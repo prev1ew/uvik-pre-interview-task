b5859db73efe0473e60ebe5336f7378c2fc6bf01
Vitalii <warcied1@gmail.com>
2022-10-08 00:17:23 +0300

    Update solution.py

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,1 +1,20 @@
-#TODO: task1
+import csv
+import json
+
+
+result = dict()
+with open('data.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for num, row in enumerate(csvreader):
+        if not num:  # if n = 0
+            continue
+        city, human_name = row
+        if city not in result:
+            result[city] = dict()
+            result[city]['people'] = list()
+        result[city]['people'].append(human_name)
+
+for row in result.values():
+    row['count'] = len(row['people'])
+
+print(json.dumps(result, indent=2, default=str))
